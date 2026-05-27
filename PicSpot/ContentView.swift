9937f3da104288e7a33872c1f11e605707c51271
jihoon <sjh971101@naver.com>
2026-05-27 23:37:46 +0900

    Initial Commit

---
+++ PicSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicSpot
+//
+//  Created by 서지훈 on 5/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicSpot/PicSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicSpotApp.swift
+//  PicSpot
+//
+//  Created by 서지훈 on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PicSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
